fb65e11f74e3f2c438cba5558d863f538d0fca86
John Iskander <john.2.face@gmail.com>
2020-11-20 15:14:02 +0100

    changing back again to command line arguments with time trigger

--- puppeteer_test.js
+++ puppeteer_test.js
@@ -1,25 +1,24 @@
 const { argv } = require('process');
 const puppeteer = require('puppeteer');
 var schedule = require('node-schedule');
-//const no_of_persons = process.argv[2];
+const no_of_persons = process.argv[2];
 var rule = new schedule.RecurrenceRule();
-//rule.dayOfWeek = [0, new schedule.Range(4, 6)];
-rule.hour = 21;
-rule.minute = 23;
+rule.dayOfWeek = [0, new schedule.Range(4, 6)];
+rule.hour = 18;
+rule.minute = 03;
 rule.tz = "CET";
-const persons = ['John Iskander','Jackline Tawadors'];
-const handynr = '015778442307';//process.argv[3];
-const email = 'john.iskander@gmx.de';//process.argv[4];
+const handynr = process.argv[3];
+const email = process.argv[4];
 
 let mytxt = `Handy ${handynr}
 email:${email}
-Reserver for :${persons.length}
+Reserver for :${no_of_persons}
 `;
-// no_of_valid_args = (no_of_persons*2)+2+3
-// if (argv.length != no_of_valid_args)
-// {
-//     console.log("Please enter all needed data ! you need to enter : (number of reservations) (Phone number) (email) {according to the number of reservations repeat the followning!}(first name of first guest) (Last name of first guest) ...");
-// }
+no_of_valid_args = (no_of_persons*2)+2+3
+if (argv.length != no_of_valid_args)
+{
+    console.log("Please enter all needed data ! you need to enter : (number of reservations) (Phone number) (email) {according to the number of reservations repeat the followning!}(first name of first guest) (Last name of first guest) ...");
+}
 
 console.log (mytxt);
 var j = schedule.scheduleJob(rule,
@@ -61,15 +60,15 @@ function() {
 
   console.log("Filling Persons Names!!");
   try{
-      for(i=0,j=5;i<persons.length;i++,j+=2)
+      for(i=0,j=5;i<no_of_persons;i++,j+=2)
       {
         console.log(`Entering Person[${i}]
-        First name : ${persons[i].split(' ')[0]}
-        Last name : ${persons[i].split(' ')[1]}`);
+        First name : ${process.argv[j]}
+        Last name : ${process.argv[j+1]}`);
         const per_vorname = await page.$(`#person\\[${i}\\]\\[firstname\\]`);
-        await per_vorname.type(persons[i].split(' ')[0]);
+        await per_vorname.type(process.argv[j+1]);
         const per_nachname = await page.$(`#person\\[${i}\\]\\[surname\\]`);
-        await per_nachname.type(persons[i].split(' ')[1]);
+        await per_nachname.type(process.argv[j+1]);
       }
   }
   catch(err)
