eac3c4048d5127e769ce04599d40863aa454432c
John Iskander <john.2.face@gmail.com>
2021-02-01 23:47:24 +0100

    adding debug print on test2.js

--- puppeteer_test2.js
+++ puppeteer_test2.js
@@ -5,6 +5,6 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://www.duolingo.com/');
   await page.screenshot({path: 'example.png'});
-
+  console.log("##[debug]screenshot been taken!");
   await browser.close();
 })();
